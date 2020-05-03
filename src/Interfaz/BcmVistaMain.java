b98610c1c10c741d9c26855bcc10e1c9ab5a7609
Arsene Lupin <keyster088@gmail.com>
2020-05-03 21:10:57 +0200

    Versión 0.2

--- src/Interfaz/BcmVistaMain.java
+++ src/Interfaz/BcmVistaMain.java
@@ -4,26 +4,49 @@
 
 package Interfaz;
 
-import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.GroupLayout;
+import java.awt.*;
+import javax.swing.border.*;
 
 /**
- * @author Khanix
+ * @author Arsene
  */
 public class BcmVistaMain extends JFrame {
     public BcmVistaMain() {
         initComponents();
     }
 
+    private void genBCMBTMouseEntered(MouseEvent e) {
+        genBCMBT.setBackground(new Color(0, 75, 126, 60));
+    }
+
+    private void genBCMBTMouseExited(MouseEvent e) {
+        genBCMBT.setBackground(new Color(0, 75, 126));
+        genBCMBT.setForeground(Color.white);
+    }
+
+    private void genBCMBTMousePressed(MouseEvent e) {
+        // TODO add your code here
+    }
+
+    private void genBCMBTMouseClicked(MouseEvent e) {
+        // TODO add your code here
+    }
+
+    private void genBCMBTMouseReleased(MouseEvent e) {
+        // TODO add your code here
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        panel1 = new JPanel();
+        pInicioPN = new JPanel();
         nombreLB = new JLabel();
         nFilasLB = new JLabel();
         nColumnasLB = new JLabel();
         nFilasTF = new JTextField();
         nColumnasTF = new JTextField();
+        genBCMBT = new JButton();
 
         //======== this ========
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -31,7 +54,7 @@ public class BcmVistaMain extends JFrame {
         setMinimumSize(new Dimension(128, 128));
         var contentPane = getContentPane();
 
-        //======== panel1 ========
+        //======== pInicioPN ========
         {
 
             //---- nombreLB ----
@@ -53,33 +76,67 @@ public class BcmVistaMain extends JFrame {
             //---- nColumnasTF ----
             nColumnasTF.setFont(new Font("Bahnschrift", Font.PLAIN, 18));
 
-            GroupLayout panel1Layout = new GroupLayout(panel1);
-            panel1.setLayout(panel1Layout);
-            panel1Layout.setHorizontalGroup(
-                panel1Layout.createParallelGroup()
+            //---- genBCMBT ----
+            genBCMBT.setText("Generar mapeado");
+            genBCMBT.setBackground(new Color(0, 75, 126));
+            genBCMBT.setForeground(Color.white);
+            genBCMBT.setBorder(new LineBorder(Color.black, 2));
+            genBCMBT.setHorizontalTextPosition(SwingConstants.CENTER);
+            genBCMBT.setFocusPainted(false);
+            genBCMBT.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    genBCMBTMouseClicked(e);
+                }
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    genBCMBTMouseEntered(e);
+                    genBCMBTMouseEntered(e);
+                }
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    genBCMBTMouseExited(e);
+                }
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    genBCMBTMousePressed(e);
+                }
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    genBCMBTMouseReleased(e);
+                }
+            });
+
+            GroupLayout pInicioPNLayout = new GroupLayout(pInicioPN);
+            pInicioPN.setLayout(pInicioPNLayout);
+            pInicioPNLayout.setHorizontalGroup(
+                pInicioPNLayout.createParallelGroup()
                     .addComponent(nombreLB, GroupLayout.DEFAULT_SIZE, 398, Short.MAX_VALUE)
-                    .addGroup(panel1Layout.createSequentialGroup()
+                    .addGroup(pInicioPNLayout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(nFilasLB)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(nFilasTF, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(nFilasTF, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(nColumnasLB)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(nColumnasTF, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(185, Short.MAX_VALUE))
+                        .addComponent(nColumnasTF, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                        .addComponent(genBCMBT, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
             );
-            panel1Layout.setVerticalGroup(
-                panel1Layout.createParallelGroup()
-                    .addGroup(panel1Layout.createSequentialGroup()
+            pInicioPNLayout.setVerticalGroup(
+                pInicioPNLayout.createParallelGroup()
+                    .addGroup(pInicioPNLayout.createSequentialGroup()
                         .addComponent(nombreLB)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addGap(8, 8, 8)
+                        .addGroup(pInicioPNLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                             .addComponent(nFilasLB)
-                            .addComponent(nFilasTF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(nFilasTF, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
                             .addComponent(nColumnasLB)
-                            .addComponent(nColumnasTF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 22, Short.MAX_VALUE))
+                            .addComponent(nColumnasTF, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(genBCMBT, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 16, Short.MAX_VALUE))
             );
         }
 
@@ -87,12 +144,12 @@ public class BcmVistaMain extends JFrame {
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
-                .addComponent(panel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(pInicioPN, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
                 .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addComponent(panel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pInicioPN, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addContainerGap(184, Short.MAX_VALUE))
         );
         pack();
@@ -101,11 +158,12 @@ public class BcmVistaMain extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    private JPanel panel1;
+    private JPanel pInicioPN;
     private JLabel nombreLB;
     private JLabel nFilasLB;
     private JLabel nColumnasLB;
     private JTextField nFilasTF;
     private JTextField nColumnasTF;
+    private JButton genBCMBT;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
